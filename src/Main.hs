eaed5a0e2e039d7b2629b5e30a9ce20d089607cf
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-30 01:27:29 -0500

    asd

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,6 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, OverloadedStrings, EmptyDataDecls #-}
 {-# LANGUAGE GADTs, FlexibleContexts #-}
 module Main where
-import Control.Monad (forM_)
-import Text.Blaze.Html5
-import Text.Blaze.Html5.Attributes
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import Text.Blaze.Html.Renderer.Text
-  
 import Database.Persist
 import Database.Persist.Sqlite
 import Database.Persist.TH
@@ -27,6 +20,7 @@ import qualified Data.ByteString as BS
 import System.Environment
 import Obvious.Model
 import Obvious.Util
+import Obvious.Views
 import qualified Network.HTTP.Types as HTTP
 import Data.Time (getCurrentTime)
 import Data.Monoid (mconcat)
@@ -37,7 +31,7 @@ import Database.Persist.Store
 import Data.Int
 
 readPosts :: IO [Post]
-readPosts = Prelude.map entityVal <$> (runDb $ selectList [PostDraft ==. False] [LimitTo 10])
+readPosts = map entityVal <$> (runDb $ selectList [PostDraft ==. False] [LimitTo 10])
 
 getKey :: Int64 -> Key SqlPersist Post
 getKey postId = (Key (PersistInt64 postId))
@@ -112,60 +106,4 @@ main = do
     S.get "/admin" $ do
       S.html "TODO"
 
-
-
-
-blaze = S.html . renderHtml
-
-for_ = flip Prelude.map
-
-wrap content = blaze $ do
-  html $ do
-    H.head $ do
-      H.title "My Blog"
-    body $ do
-      H.div ! A.id "content" $ do
-        content
-
-renderPost post = do
-  h1 . toHtml $ postTitle post
-  p . toHtml $ postContent post
-
-renderPosts posts = do
-  mconcat $ for_ posts $ renderPost
-
-editPost post postId = do
-  h1 "Edit Post"
-  H.form ! method "post" ! action "/update" $ do
-    input ! value (toValue postId) ! type_ "hidden" ! name "id"
-    H.label "Title" ! for "title"
-    input ! value (toValue . postTitle $ post) ! type_ "text" ! A.id "title" ! name "title"
-    H.label "Content" ! for "content"
-    H.textarea (toHtml . postContent $ post) ! A.id "content" ! name "content"
-    H.label "Draft?" ! for "draft"
-    if (postDraft post)
-      then input ! type_ "checkbox" ! A.id "draft" ! name "draft" ! checked "checked"
-      else input ! type_ "checkbox" ! A.id "draft" ! name "draft"
-    H.label "Aside?" ! for "aside"
-    if (postAside post)
-      then input ! type_ "checkbox" ! A.id "aside" ! name "aside" ! checked "checked"
-      else input ! type_ "checkbox" ! A.id "aside" ! name "aside"    
-    H.label "Url" ! for "url"
-    input ! value (toValue . postUrl $ post) ! type_ "text" ! A.id "url" ! name "url"
-    input ! type_ "submit"    
-
-newPost = do
-  h1 "New Post"
-  H.form ! method "post" ! action "/create" $ do
-    H.label "Title" ! for "title"
-    input ! type_ "text" ! A.id "title" ! name "title"
-    H.label "Content" ! for "content"
-    H.textarea "Content here" ! A.id "content" ! name "content"
-    H.label "Draft?" ! for "draft"
-    input ! type_ "checkbox" ! A.id "draft" ! name "draft"
-    H.label "Aside?" ! for "aside"
-    input ! type_ "checkbox" ! A.id "aside" ! name "aside"
-    H.label "Url" ! for "url"
-    input ! type_ "text" ! A.id "url" ! name "url"
-    input ! type_ "submit"
 
