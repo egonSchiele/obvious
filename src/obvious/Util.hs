4dd4055379f5614455612969553bcc5d1bfe70aa
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-30 01:53:16 -0500

    sfs

--- src/obvious/Util.hs
+++ src/obvious/Util.hs
@@ -1,9 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
 
 module Obvious.Util where
 import Database.Persist
-import Database.Persist.Sqlite
 import Data.Text 
+
+-- uncomment this on dev
+-- #define SQLite
+
+#ifdef SQLite
+
+import Database.Persist.Sqlite
 
 runDb :: SqlPersist IO a -> IO a
 runDb = withSqliteConn "dev.sqlite3" . runSqlConn
+
+#else
+
+import           Data.Monoid                 ((<>))
+import           Data.Text.Encoding          (encodeUtf8)
+import           Database.Persist.Postgresql (withPostgresqlConn)
+import           Database.Persist.Store      (applyEnv, loadConfig)
+import           Web.Heroku                  (dbConnParams)
+
+runSql :: SqlPersist IO a -> IO a
+runSql query = do
+    params <- dbConnParams
+    let connStr = foldr (\(k,v) t -> t <> (encodeUtf8 $ k <> "=" <> v <> " "))
+                  "" params
+    withPostgresqlConn connStr $ runSqlConn query
+
+#endif
